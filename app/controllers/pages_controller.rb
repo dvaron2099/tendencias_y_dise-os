77ce83dd8280fb4e05370de08b0ccffa7080a046
david varon <varondavid20@gmail.com>
2023-10-10 02:24:21 -0500

    arregalndo x6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,7 @@ class PagesController < ApplicationController
 
     # Asigna los productos aleatorios a la variable @initial para que estén disponibles en la vista.
     @initial = random_products
+    @first_two_products = @initial.first(2)
+    @last_two_products = @initial.last(2)
   end
 end
